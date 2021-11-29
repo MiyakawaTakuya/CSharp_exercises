e73594a89e6abf091027380154e9c12842b646b4
Miyakawa Takuya <omyakawa@gmail.com>
2021-11-29 23:43:12 +0900

    Paiza会に向けて初挑戦. C005

---
+++ paiza_C/C005.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+namespace paiza_C
+{
+    public class C005
+    {
+        internal static void test()
+        {
+            int i = 0;
+            string[] array;
+            var list = new List<string>();
+            while (true)
+            {
+                string line = System.Console.ReadLine();
+                if (line == null)
+                {
+                    break;
+                }
+                else
+                {
+                    list.Add(line);
+                    i++;
+                }
+            }
+            array = list.ToArray();
+            Console.WriteLine(array[0]);
+            Console.WriteLine(array[1]);
+            Console.WriteLine(array[2]);
+            Console.WriteLine(array[3]);
+            Console.WriteLine(array[4]);
+        }
+
+    }
+}
