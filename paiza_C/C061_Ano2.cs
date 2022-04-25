bca08e7e2cbf5a3dcf1ce165ccc39e68909eedef
Miyakawa Takuya <omyakawa@gmail.com>
2022-04-26 01:07:23 +0900

    try refactoring C061

--- paiza_C/C061_Ano2.cs
+++ paiza_C/C061_Ano2.cs
@@ -1,19 +1,33 @@
 ﻿// using System.Collections.Generic;
 using System.Linq;
 using System;
-//C061:繰り上がりのない足し算  220425 
+//C061:繰り上がりのない足し算  
+//受験結果 受験言語： C# 獲得ランク： Cランク スコア： 100点  220425
 //"ones place"...一の位、”tens place"...十の位、”hundreds place"...百の位
+//func string.Substring https://itsakura.com/csharp-substring
 
 namespace paiza_C
 {
     public class C061_Ano2
     {
-		//各位の１文字を受け取って一時的に数値に置き換えて足して、一の位だけをstringで返す関数
-		internal static string SumEachDigit(string numA_, string numB_)
+		internal static void main()
 		{
-			string numA = normalizeThreeDigit(numA_);
-			string numB = normalizeThreeDigit(numB_);
+			string[] strArray = Console.ReadLine().Trim().Split(' ');
+			Console.WriteLine(SumEachDigit(strArray[0], strArray[1]));
+		}
 
+		internal static string SumEachDigit(string _numA, string _numB)
+		{
+			int maxDigit = Math.Max(_numA.Length, _numB.Length);  //get biggest digit in two intger.
+			string numA = normalizeThreeDigit(_numA);
+			string numB = normalizeThreeDigit(_numB);
+			string answer =
+				addWithoutCarryforward(numA.Substring(0, 1), numB.Substring(0, 1))
+				+ addWithoutCarryforward(numA.Substring(1, 1), numB.Substring(1, 1))
+				+ addWithoutCarryforward(numA.Substring(2, 1), numB.Substring(2, 1));
+			if (maxDigit == 1) answer = answer[2].ToString();
+			else if (maxDigit == 2) answer = answer[1].ToString() + answer[2].ToString();
+			return answer;
 		}
 
 		internal static string normalizeThreeDigit(string str)
@@ -25,23 +39,10 @@ namespace paiza_C
 		internal static string addWithoutCarryforward(string a, string b)
 		{
 			int sum = int.Parse(a) + int.Parse(b);
-			if (sum >= 10)
-            {
-                return sum.ToString().Substring(1, 1);  
-            }
-            else
-            {
-                return sum.ToString();  //足しても一桁だったらそのまま返す
-            }
-
+			if (sum >= 10) return sum.ToString().Substring(1, 1);  //if two-digit intger, slice one-digit 
+			else return sum.ToString();  
 		}
 
-		internal static void main()
-		{
-			string[] strArray = Console.ReadLine().Trim().Split(' ');
-			string answer = SumEachDigit(strArray[0], strArray[1]);
 
-			Console.WriteLine();
-		}
 	}
 }
